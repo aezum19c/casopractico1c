101e324a0cff8dd82446e69b1be773845a5c6501
aezum19c <aezum19c@gmail.com>
2024-02-12 21:59:59 -0500

    Agregar fichero sh 2

--- testRest.sh
+++ testRest.sh
@@ -7,7 +7,16 @@ echo $status_code
 if [ $status_code = 200 ]
 then
     echo "URL correcta"
-    exit 1
+    echo "Validar busqueda de codigo inexistente"
+    status_code=$(curl -s -o /dev/null -w "%{http_code}"  https://r89xg1nxpf.execute-api.us-east-1.amazonaws.com/Prod/todos/codigo_inexistente)
+    if [ $status_code = 404 ]
+    then
+        echo "El codigo no existe"
+    else
+        echo "El endpoint no funciona correctamente"
+        exit 1
+    fi
 else
     echo "La url no es correcta"
+    exit 1
 fi
